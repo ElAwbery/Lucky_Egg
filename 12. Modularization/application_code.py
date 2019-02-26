36a389c8efb687f8ad4bcb509e066ae4bcad8f9f
ElAwbery <36336917+ElAwbery@users.noreply.github.com>
2019-02-26 09:57:38 -0800

    Add files via upload

---
+++ 12. Modularization/application_code.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 16:33:07 2019
+
+@author: Charlie
+"""
+
+"""
+Application_code module constructs the web page view. 
+
+Application code consists of two ORM_object subclasses, pokemon_family and 
+pokemon_species.
+
+The pokemon_family subclass keeps track of candy counts and family members 
+which are pokemon_species objects. 
+
+The pokemon_species subclass keeps track of individual pokemon counts. 
+
+The Pokemon database mirrors the subclass structure with two tables. 
+    
+The pokemon_species subclass has data attributes UID, family, name and count 
+and the pokemon_family subclass has data attributes UID (a family ID distinct from
+the UID of the individual pokemon species), candies, baby, first_stage, 
+second_stage and third_stage. 
+
+The ORM_object class relies on table names in the database being identical to
+its subclass names and the column headings in the tables being identical to the
+subclass data attributes.
+
+Thus our subclass data attributes are identical to our database table 
+column names.
+
+The family column in the pokemon_species table contains a family UID that is
+a foreign key. The pokemon stages columns in the pokemon_family table 
+have foreign key constraints to individual species' UIDs in the pokemon_species 
+table.   
+
+Our application code has been updated to call the server and talk to it with an 
+HTTPRequestHandler subclass,'pokemon_handler'. Its get_router and post_router 
+methods handle application specific page construction. These constitute the
+controller of the MVC architecture.
+"""
+
+import database_ORM as ORM
+import server
+import template
+
+
+class pokemon_family(ORM.ORM_object):
+    """
+    Keeps track of family pokemon stages and candies per family
+    """
+  
+    def set_candies(self, candies):
+        """
+        candies, a positive integer
+        """
+        assert type(candies) == int
+        self.candies = candies
+        self.store()
+   
+       
+class pokemon_species(ORM.ORM_object):
+    """
+    pokemon_species data attributes are used to construct the body of a web 
+    page representing that species
+    keeps track of species counts
+    """
+    # The class needs a specialized init procedure because the status line 
+    # is not stored in the database. It must be instantiated here.
+    def __init__(self, ID): 
+        ORM.ORM_object.__init__(self, ID)
+        
+        self.status_line = ""
+        
+    def set_pokemon_count(self, new_count):
+        """
+        New_count is an int
+        Updates Pokemon count and saves to database
+        """
+        assert type(new_count) == int
+        self.count = new_count
+        self.store()
+    
+    def get_html(self):
+        """
+        Identifies the stage of pokemon_species using the pokemon_family 
+        class attributes
+        Returns html variable for the stage
+        """
+             
+        # Every family has a first stage, so we don't need to null-check here
+        if self == self.family.first_stage:
+            html = template.substitute(self, '''
+    <h1>$name$</h1>
+    $status_line$
+    <p>$name$ is a first stage Pokemon.</p>
+    <p>$name$ evolves into a <a href = "/$family.second_stage.name$">$family.second_stage.name$</a></p>
+    <p>Its third stage is a <a href = "/$family.third_stage.name$">$family.third_stage.name$</a></p>
+    
+    <form action="$name$" method="post">
+      <p>You have 
+        <input type="number" name="new_count" value = "$count$" min="0"> $name$s
+        and
+        <input type="number" name="new_candies" value = "$family.candies$" min="0"> $name$ candies.
+      </p>
+      <button type="submit">Update</button>
+    </form>
+    ''')
+        
+        elif self.family.second_stage != None and self == self.family.second_stage:
+            html = template.substitute(self, '''
+    <h1>$name$</h1>
+    $status_line$
+    <p>$name$ is a second stage Pokemon.</p> 
+    <p>$name$ evolves from a <a href = "/$family.first_stage.name$">$family.first_stage.name$</a></p> 
+    <p>Its third stage is a <a href = "/$family.third_stage.name$">$family.third_stage.name$</a><br></p> 
+    <p>$name$ uses <a href = "/$family.first_stage.name$">$family.first_stage.name$</a> candies. You have $family.candies$ of them.<br></p> 
+    
+    <form action="$name$" method="post">                     
+      <p>You have 
+        <input type="number" name="new_count" value = "$count$" min="0"> $name$s
+      </p>
+      <button type="submit">Update</button>
+    </form>
+    ''')   
+            
+        elif self.family.third_stage != None and self == self.family.third_stage:
+            html = template.substitute(self, '''
+    <h1>$name$</h1>
+    $status_line$
+    <p>$name$ is a third stage Pokemon.</p>
+    <p>$name$ evolves from a <a href = "/$family.second_stage.name$">$family.second_stage.name$</a></p>
+    <p>Its first stage is a <a href = "/$family.first_stage.name$">$family.first_stage.name$</a></p>
+    <p>$name$ uses <a href = "/$family.first_stage.name$">$family.first_stage.name$</a> candies. You have $family.candies$ of them.</p>
+    
+    <form action="$name$" method="post">                     
+      <p>You have 
+        <input type="number" name="new_count" value = "$count$" min="0"> $name$s
+      </p>
+      <button type="submit">Update</button>
+    </form>
+    ''')
+            
+        elif self.family.baby != None and self == self.family.baby:
+            html = template.substitute(self, '''
+    <h1>$name$</h1>
+    $status_line$
+    <p>$name$ is a baby Pokemon.</p>
+    <p>$name$ evolves into a <a href = "/$family.first_stage.name$">$family.first_stage.name$</a></p>
+    <p>Its second stage is a <a href = "/$family.second_stage.name$">$family.second_stage.name$</a></p>
+    <p>Its third stage is a <a href = "/$family.third_stage.name$">$family.third_stage.name$</a></p>
+    
+    <form action="$name$" method="post">
+      <p>You have 
+        <input type="number" name="new_count" value = "$count$" min="0"> $name$s
+        and
+        <input type="number" name="new_candies" value = "$family.candies$" min="0"> $name$ candies.
+      </p>
+      <button type="submit">Update</button>
+    </form>
+    ''')  
+        else:
+            raise AttributeError ("Problem identifying pokemon species stage.") 
+          
+        return html
+
+
+class pokemon_handler(server.applicationHandler):
+    """
+    Handles application-specific web page construction and user actions
+    get_router and post_router methods tell the server how to deal with requests
+    """
+
+    # Boilerplate prefix and suffix for an html page
+    boilerplate_prefix = '''<!DOCTYPE html>
+<html lang="en">
+ <head>
+   <meta charset="utf-8">
+   <link href="pokemon_page.css" rel="stylesheet" type="text/css"/>
+ </head>
+ <body>
+'''
+    boilerplate_suffix = '''
+ </body>
+</html>'''
+    
+    def get_router(self):
+        """
+        Generates a GET request response, e.g. from html boilerplate plus species-specific html.
+        Returns a tuple, content_type (eg: html or css) and contents.
+        If url not found, returns string for 404 error message. 
+        """
+        path = self.path
+        page_name = path[1:]         # strip the leading slash
+      
+        # Response for CSS request
+        if page_name == "pokemon_page.css":
+            
+            content_type = 'text/css'
+            
+            contents = '''.status_line {
+              margin:30px;
+              border:2px solid #006600;
+              padding:10px;
+              font-family:Constantia,Georgia,"Times New Roman","Times Roman", Times,TimesNR,serif;
+              font-size:25px;
+              color:#006600; 
+              text-align:center; 
+              background-color:#dbffb6; 
+              width:450px;
+             }
+            h1 {
+              font-family:Constantia,Georgia,"Times New Roman","Times Roman",
+              Times,TimesNR,serif;
+              font-size:40px; font-style:italic;
+            }
+            input {
+              width:50px;
+            }'''
+            
+        # if the path is the name of a known pokemon, get its html string and 
+        # construct the response:
+        else:
+            try:
+                loaded = ORM.value_to_ORM_object('pokemon_species', 'Name', page_name)
+                content_type = "text/html"
+                contents = self.boilerplate_prefix + loaded.get_html() + self.boilerplate_suffix
+              
+            except:
+                content_type = "error"
+                contents = "Pokemon species not found."
+                
+        return (content_type, contents)
+
+
+    def post_router(self):
+        
+        '''
+        Processes a POST request to update the count of pokemons and/or candies 
+        of a species.
+        Returns a success page.
+        '''
+        path = self.path
+        page_name = path[1:]    # strip leading slash
+        arguments = self.arguments
+        
+        # Update data in the pokemon class
+        try:
+            
+            loaded = ORM.value_to_ORM_object('pokemon_species', 'Name', page_name)
+            
+            if "new_candies" in arguments:
+                loaded.family.set_candies(int(arguments["new_candies"]))
+                            
+            # Every update sets the count, whereas only first-stage updates 
+            # set the candies
+            loaded.set_pokemon_count(int(arguments["new_count"]))
+            loaded.status_line = '''<p class="status_line">Updated</p>'''
+            content_type = "text/html"
+            contents = self.boilerplate_prefix + loaded.get_html() + self.boilerplate_suffix
+            
+            # Clear the status line so that it does not show as
+            # updated in subsequent GET requests
+            loaded.status_line = ""
+                
+        # 404 error: load method failed
+        except:
+            content_type = "error"
+            contents = "Could not set candies for this Pokemon family."
+            
+        return (content_type, contents)
+ 
+# Call the server to run our application       
+server.web_server(pokemon_handler)
---
+++ 12. Modularization/server.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 15:17:03 2019
+
+@author: Charlie
+"""
+import http.server
+import socketserver
+
+"""
+Server code has been separated from the application code: in previous versions
+they were entangled and the do_GET and do_POST handlers were pokemon-specific.
+In principle this version can process any browser request and call the  
+application's router methods.
+The web server connection is wrapped in a web_server function called by 
+the application code, and no longer runs on loading. 
+"""
+
+PORT = 80
+
+    
+# Source code for the HTTPRequestHandler class: 
+# https://github.com/python/cpython/blob/2.7/Lib/BaseHTTPServer.py
+# "  - rfile is a file object open for reading positioned at the start of 
+# the optional input data part "
+               
+# Python documentation for working with streams: 
+# https://docs.python.org/3/library/io.html
+
+"""
+The applicationHandler class is a generic interface between the browser and 
+our application code. 
+"""
+
+class applicationHandler(http.server.SimpleHTTPRequestHandler):
+    """
+    Sends get and post requests to application code
+    Sends pages to the web browser
+    """
+    
+    def parse_http_arguments(self, string):
+        """
+        Takes a single string of user input from an HTML form
+        The string is keyword=value pairs separated by &s
+        Returns a dictionary of the keyword/value pairs
+        """
+        # create a list with two elements
+        new_data = {}
+        key_value_pairs = string.split('&')
+        
+        for pair in key_value_pairs:
+            key_value = pair.split('=')
+            key = key_value[0]
+            value = key_value[1]
+            if value == '':
+                value = 0
+            new_data[key] = value
+        
+        return(new_data)
+        
+    
+    def do_GET(self):
+        """
+        Called by the server when the browser sends a GET request
+        Gets content_type (which goes in the header: html, css or error message) 
+        and response contents from the application's router
+        Sends that to browser
+        """
+    
+        (content_type, data) = self.get_router()
+        
+        assert type(content_type) == str and type(data) == str
+        
+        if content_type == 'error':
+            self.send_error(http.HTTPStatus.NOT_FOUND, data.format(self.path))
+
+        else:
+            self.send_response(http.HTTPStatus.OK)
+            self.send_header("Content-type", content_type)
+            self.end_headers()
+            self.wfile.write(data.encode('utf-8')) 
+        
+    def do_POST(self):
+        """
+        Called by the server when the browser sends a POST request
+        Cleans up request and sends its contents to the application's router
+        Sends application's response to the browser
+        """
+        
+        # self.rfile is the data string. It's an io.BufferedReader object 
+        # (a binary object).
+        # io.BufferedReader has a read method that requires a length argument.
+        # self.headers gets the http headers as a dict. Content-Length is one 
+        # of the headers, a dict key, the length of the data string. 
+        # All header values are strings, so we coerce it to an int.
+        # The read method returns a python bytes object, not a python string, 
+        # so we have to convert it using the decode method.
+        
+        user_update = self.rfile.read(int(self.headers['Content-Length'])).decode('utf-8')
+        
+        self.arguments = self.parse_http_arguments(user_update)
+        
+        (content_type, page) = self.post_router()
+        
+        if content_type == 'error':
+            self.send_error(http.HTTPStatus.NOT_FOUND, page.format(self.path))
+        
+        else: 
+            self.send_response(http.HTTPStatus.OK)
+            self.send_header("Content-type", content_type)
+            self.end_headers()
+            self.wfile.write(page.encode('utf-8'))
+            
+# set up an OS connection at PORT 80, specify the handler for the port, wait for a url request 
+def web_server(handler):
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print("serving at port", PORT)
+        # an infinite loop of waiting for a request
+        httpd.serve_forever()
+
+        
